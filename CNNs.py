7c0da8918d7d877863447acd47e47277c7270d5b
Olutomilayo Amazing-Grace <41172482+AmazingGrace-D@users.noreply.github.com>
2020-04-27 21:28:51 +0100

    Update CNNs.py

--- CNNs.py
+++ CNNs.py
@@ -20,6 +20,7 @@ classifier.add(Convolution2D(32, kernel_size = (3, 3), strides = (1, 1), input_s
 # Step 2 - Pooling
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
+# Adding a second convolutional layer
 classifier.add(Convolution2D(32, kernel_size = (3, 3), strides = (1, 1), padding = 'valid', activation = 'relu'))
 classifier.add(MaxPooling2D(pool_size = (2, 2)))
 
